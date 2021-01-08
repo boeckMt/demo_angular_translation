ae865e9ad1a053184cb35a0d7f023ea2931295ac
Langbein, Michael <michael.langbein@dlr.de>
2021-01-08 12:29:14 +0100

    basic i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LanguagePickerComponent } from './language-picker/language-picker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LanguagePickerComponent
   ],
   imports: [
     BrowserModule
