6fb06dc00575173b39a1007bf1f3d83ae901aa3c
Mathias Boeck <Mathias.Boeck@dlr.de>
2021-01-11 16:28:23 +0100

    translate app name and adjust translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'translateMe';
+  title = $localize`:@@app.title: translateMe-App`;
   minutes = 1;
   gender = 'male';
 }
