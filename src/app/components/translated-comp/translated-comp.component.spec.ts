7dc7a776635c10b3faf2fb9ef35ebf6c2bccdd2d
Mathias Boeck <Mathias.Boeck@dlr.de>
2021-01-11 15:18:31 +0100

    update angular

--- src/app/components/translated-comp/translated-comp.component.spec.ts
+++ src/app/components/translated-comp/translated-comp.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TranslatedCompComponent } from './translated-comp.component';
 
@@ -6,7 +6,7 @@ describe('TranslatedCompComponent', () => {
   let component: TranslatedCompComponent;
   let fixture: ComponentFixture<TranslatedCompComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TranslatedCompComponent ]
     })
